8a26add70103e1b6db740a4e052a838b60b1a598
Brayden Perry <brayden.l.perry@gmail.com>
2022-02-08 10:23:28 -0500

    added auto date update

--- ReadMeUpdater.py
+++ ReadMeUpdater.py
@@ -1,9 +1,21 @@
 #import subprocess
+import time
+from datetime import date
 
 readFile = open("README.md","r")
-#writeFile = open("README.md","w")
-string = ""
+contents = []
 for line in readFile:
-	string = string + line
+	contents.append(line.strip())
+readFile.close()
+writeFile = open("README.md","w")
+
 	
-print(string)
+for line in contents:
+	print(line)
+while True:
+	if contents[2] != "Today's Date is: " + date.today():
+		contents[2] = "Today's Date is: " + date.today()
+		writeFile.write(contents[0])
+		writeFile.write(contents[1])
+		writeFile.write(contents[2])
+	time.sleep(86400)
