1a3e45f4dcdc7e98623832eb2bf405d4456505f6
Brayden Perry <brayden.l.perry@gmail.com>
2022-02-08 10:26:25 -0500

    updated to new format

--- ReadMeUpdater.py
+++ ReadMeUpdater.py
@@ -13,9 +13,10 @@ writeFile = open("README.md","w")
 for line in contents:
 	print(line)
 while True:
-	if contents[2] != "Today's Date is: " + date.today():
+	if contents[3] != "Today's Date is: " + date.today():
 		contents[2] = "Today's Date is: " + date.today()
 		writeFile.write(contents[0])
 		writeFile.write(contents[1])
 		writeFile.write(contents[2])
+		writeFile.write(contents[3])
 	time.sleep(86400)
