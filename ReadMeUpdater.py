c0487dc171602e8c9de5ae6a9e58bb6809f625c6
Brayden Perry <brayden.l.perry@gmail.com>
2022-02-08 10:45:35 -0500

    beginning subprosses integration

--- ReadMeUpdater.py
+++ ReadMeUpdater.py
@@ -1,22 +1,31 @@
-#import subprocess
+import subprocess
 import time
 from datetime import date
 
 readFile = open("README.md","r")
 contents = []
 for line in readFile:
-	contents.append(line.strip())
+	contents.append(line)
 readFile.close()
-writeFile = open("README.md","w")
 
+subprocess["ssh-agent"]
+subprocess["ssh", "-i","subp","git@github.com"]
+subprocess["bye"]
 	
 for line in contents:
 	print(line)
 while True:
-	if contents[3] != "Today's Date is: " + date.today():
-		contents[2] = "Today's Date is: " + date.today()
+	current = str(date.today())
+	if contents[3] != "Today's Date is: " + current:
+		writeFile = open("README.md","w")
+		contents[3] = "Today's Date is: " + current
 		writeFile.write(contents[0])
 		writeFile.write(contents[1])
 		writeFile.write(contents[2])
 		writeFile.write(contents[3])
+		print(contents[3])
+		writeFile.close()
+		subprocess["git","add","README.md"]
+		subprocess["git","commit","-m",'"Updated file date"']
+		subprocess["git","push"]
 	time.sleep(86400)
